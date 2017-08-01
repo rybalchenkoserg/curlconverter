a3bc0ab4a0b8e5d69c2f3c3970c065b6d9a5c678
Sergii <rybalchenkoserg@gmail.com>
2017-08-01 03:09:23 +0300

    Add options param to node generator to provide features: 1. Add gzip prop to request options 2. Parametrization of url and cookies 3. Custom code insertion:  3.1 To file header  3.2 To callback 4. Event fired up after curl parser is done.

--- generators/node.js
+++ generators/node.js
@@ -1,10 +1,37 @@
 var util = require('../util')
 var jsesc = require('jsesc')
 
-var toNode = function (curlCommand) {
+
+var toNode = function (curlCommand, options) {
+  let opt = options || {var: {cookies: false}};
   var request = util.parseCurlCommand(curlCommand)
+  let events = ('events' in opt);
+  if (events && ('afterParse' in opt.events) && (typeof opt.events.afterParse === 'function')){
+    opt.events.afterParse(request, opt);
+  }
   var nodeCode = 'var request = require(\'request\');\n\n'
+  let varUrl = ('var' in opt) && (typeof opt.var === 'object') && (opt.var.url)
+  let code = ('code' in opt);
   if (request.headers || request.cookies) {
+
+    if (code && ('begin' in opt.code) && (typeof opt.code.begin === 'string')) {
+      nodeCode += opt.code.begin
+    }
+
+    let varCookie = request.cookies && ('var' in opt) && (typeof opt.var === 'object') && (opt.var.cookies)
+    if (varCookie) {
+      var cookieString = util.serializeCookies(request.cookies)
+      nodeCode += `var cookies =  ${opt.var.cookies};\n`;
+    }
+
+    if (varUrl)  {
+      if(typeof varUrl === 'string') {
+        nodeCode += `var url =  '${opt.var.url}';\n`;
+      } else if(typeof varUrl === 'boolean'){
+        nodeCode += `var url =  '${request.url}';\n`
+      }
+    }
+
     nodeCode += 'var headers = {\n'
     var headerCount = Object.keys(request.headers).length
     var i = 0
@@ -19,23 +46,27 @@ var toNode = function (curlCommand) {
     }
     if (request.cookies) {
       var cookieString = util.serializeCookies(request.cookies)
-      nodeCode += '    \'Cookie\': \'' + cookieString + '\'\n'
+      nodeCode += varCookie ? '    \'Cookie\': `\'${cookies}\'`\n' : '    \'Cookie\': \'' + cookieString + '\'\n'
     }
     nodeCode += '};\n\n'
   }
 
   if (request.data) {
-        // escape single quotes if there are any in there
-    if (request.data.indexOf("'") > -1) {
+    // escape single quotes if there are any in there
+    if (request.data.indexOf('\'') > -1) {
       request.data = jsesc(request.data)
     }
     nodeCode += 'var dataString = \'' + request.data + '\';\n\n'
   }
 
   nodeCode += 'var options = {\n'
-  nodeCode += '    url: \'' + request.url + '\''
+  nodeCode += varUrl ? '    url: `${url}`' : '    url: \'' + request.url + '\''
   if (request.method !== 'get') {
     nodeCode += ',\n    method: \'' + request.method.toUpperCase() + '\''
+  }
+
+  if('reqOptions' in opt && 'gzip' in opt.reqOptions && !!opt.reqOptions.gzip){
+    nodeCode += ',\n    gzip: true'
   }
 
   if (request.headers || request.cookies) {
@@ -52,8 +83,8 @@ var toNode = function (curlCommand) {
     var user = splitAuth[0] || ''
     var password = splitAuth[1] || ''
     nodeCode += '    auth: {\n'
-    nodeCode += "        'user': '" + user + "',\n"
-    nodeCode += "        'pass': '" + password + "'\n"
+    nodeCode += '        \'user\': \'' + user + '\',\n'
+    nodeCode += '        \'pass\': \'' + password + '\'\n'
     nodeCode += '    }\n'
   } else {
     nodeCode += '\n'
@@ -61,10 +92,18 @@ var toNode = function (curlCommand) {
   nodeCode += '};\n\n'
 
   nodeCode += 'function callback(error, response, body) {\n'
-  nodeCode += '    if (!error && response.statusCode == 200) {\n'
-  nodeCode += '        console.log(body);\n'
-  nodeCode += '    }\n'
+  if (code && ('callback' in opt.code) && (typeof opt.code.callback === 'string')) {
+    nodeCode += opt.code.callback
+  }
+  else {
+    nodeCode += '    if (!error && response.statusCode == 200) {\n'
+    nodeCode += '        console.log(body);\n'
+    nodeCode += '    }\n'
+  }
   nodeCode += '}\n\n'
+  if (code && ('beforeRequest' in opt.code) && (typeof opt.code.beforeRequest === 'string')) {
+    nodeCode += opt.code.beforeRequest
+  }
   nodeCode += 'request(options, callback);'
 
   return nodeCode + '\n'
