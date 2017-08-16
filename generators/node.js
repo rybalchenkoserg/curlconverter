9b5c0bd15a681fa4621c931d5150057f2e9ba482
Rybalchenko Serg <rybalchenkoserg@gmail.com>
2017-08-16 20:00:12 +0300

    Add query string parameterization Remove unwanted cookie quotes

--- generators/node.js
+++ generators/node.js
@@ -1,33 +1,42 @@
+/* eslint-disable no-template-curly-in-string */
+
 var util = require('../util')
 var jsesc = require('jsesc')
-
+var urlUtil = require('url')
+var queryString = require('querystring')
 
 var toNode = function (curlCommand, options) {
-  let opt = options || {var: {cookies: false}};
+  let opt = options || {var: {cookies: false}}
   var request = util.parseCurlCommand(curlCommand)
-  let events = ('events' in opt);
-  if (events && ('afterParse' in opt.events) && (typeof opt.events.afterParse === 'function')){
-    opt.events.afterParse(request, opt);
+  let events = ('events' in opt)
+  if (events && ('afterParse' in opt.events) && (typeof opt.events.afterParse === 'function')) {
+    opt.events.afterParse(request, opt)
   }
   var nodeCode = 'var request = require(\'request\');\n\n'
   let varUrl = ('var' in opt) && (typeof opt.var === 'object') && (opt.var.url)
-  let code = ('code' in opt);
+  let code = ('code' in opt)
   if (request.headers || request.cookies) {
-
     if (code && ('begin' in opt.code) && (typeof opt.code.begin === 'string')) {
       nodeCode += opt.code.begin
     }
 
     let varCookie = request.cookies && ('var' in opt) && (typeof opt.var === 'object') && (opt.var.cookies)
     if (varCookie) {
-      var cookieString = util.serializeCookies(request.cookies)
-      nodeCode += `var cookies =  ${opt.var.cookies};\n`;
+      nodeCode += `var cookies =  ${opt.var.cookies};\n`
     }
 
-    if (varUrl)  {
-      if(typeof varUrl === 'string') {
-        nodeCode += `var url =  '${opt.var.url}';\n`;
-      } else if(typeof varUrl === 'boolean'){
+    if (varUrl) {
+      if (typeof varUrl === 'string') {
+        nodeCode += `var url =  '${opt.var.url}';\n`
+      } else if (typeof varUrl === 'boolean') {
+        if ('urlQuery' in opt.var && typeof opt.var.urlQuery === 'boolean') {
+          let urlComponents = urlUtil.parse(request.url)
+          let queryParams = queryString.parse(urlComponents.query)
+          nodeCode += `var queryParams = ${JSON.stringify(queryParams, null, 2)};\n`
+        //  nodeCode += 'var url = '
+        } else {
+         // nodeCode += `var url =  '${request.url}';\n`
+        }
         nodeCode += `var url =  '${request.url}';\n`
       }
     }
@@ -46,7 +55,7 @@ var toNode = function (curlCommand, options) {
     }
     if (request.cookies) {
       var cookieString = util.serializeCookies(request.cookies)
-      nodeCode += varCookie ? '    \'Cookie\': `\'${cookies}\'`\n' : '    \'Cookie\': \'' + cookieString + '\'\n'
+      nodeCode += varCookie ? '    \'Cookie\': `${cookies}`\n' : '    \'Cookie\': \'' + cookieString + '\'\n'
     }
     nodeCode += '};\n\n'
   }
@@ -65,7 +74,7 @@ var toNode = function (curlCommand, options) {
     nodeCode += ',\n    method: \'' + request.method.toUpperCase() + '\''
   }
 
-  if('reqOptions' in opt && 'gzip' in opt.reqOptions && !!opt.reqOptions.gzip){
+  if ('reqOptions' in opt && 'gzip' in opt.reqOptions && !!opt.reqOptions.gzip) {
     nodeCode += ',\n    gzip: true'
   }
 
@@ -94,8 +103,7 @@ var toNode = function (curlCommand, options) {
   nodeCode += 'function callback(error, response, body) {\n'
   if (code && ('callback' in opt.code) && (typeof opt.code.callback === 'string')) {
     nodeCode += opt.code.callback
-  }
-  else {
+  } else {
     nodeCode += '    if (!error && response.statusCode == 200) {\n'
     nodeCode += '        console.log(body);\n'
     nodeCode += '    }\n'
